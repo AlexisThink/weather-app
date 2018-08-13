9fd265a6ca81b1403440641dbfa515ae5cffc76a
Alexis Olveres <alexis.olv.developer@gmail.com>
2018-08-13 11:32:39 -0500

    API Calls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import request from 'superagent'; //npm install --save superagent
 import './App.css'
 
 class App extends Component {
@@ -6,6 +7,14 @@ class App extends Component {
     super();
 
     this.state = {
+      cities: [
+        {id: 1,
+        name: 'France',
+        cords: {lat: 46.227638, lon: 2.213749}},
+        {id: 2,
+        name: 'Canada',
+        cords: {lat: 56.130366, lon: -106.346771}}
+        ],
       show: false
     }
   }
@@ -17,6 +26,26 @@ class App extends Component {
     })
   }
 
+  //For this API enable CORS
+  getFranceWeather = (e) =>{
+    e.preventDefault();
+    
+    const apiURL = 'https://api.darksky.net/forecast/2f951a609b541b2049d0bf23f70143d3/46.227638,2.213749'
+    
+    request
+    .get(apiURL)
+    .then(response => console.log(response));    
+  }
+
+  getCanadaWeather = (e) =>{
+    e.preventDefault();
+    
+    const apiURL = 'https://api.darksky.net/forecast/2f951a609b541b2049d0bf23f70143d3/56.130366,-106.346771'
+    
+    request
+    .get(apiURL)
+    .then(response => console.log(response));   
+  }
   textFind = () =>{
     console.log('Estas escribiendo')
   }
@@ -32,7 +61,8 @@ class App extends Component {
       <div className='grid'>
         <aside className='app__aside'>
           <h1 className='app__title'>All countries</h1>
-          <a href='#' className='app__country'>France</a>
+          <a onClick={this.getFranceWeather}  href='#' className='app__country'>France</a>
+          <a onClick={this.getCanadaWeather}  href='#' className='app__country'>Canada</a>
 
           {this.state.show && <input autoFocus onKeyUp={this.textFind} type='text' placeholder='Location' className='app__input' />}
         </aside>
